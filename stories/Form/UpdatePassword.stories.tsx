fb5a7258e933d75975a5e4714bf7b97421687e0d
ciscutiez <francischarlesi.gonzales@gmail.com>
2024-12-18 18:18:14 +0800

    feat: update password form storybook

---
+++ stories/Form/UpdatePassword.stories.tsx
@@ -0,0 +1,26 @@
+import UpdatePasswordForm from '@/components/Form/UpdatePassword';
+
+
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof UpdatePasswordForm> = {
+  title: 'Forms/Update Password Form',
+  component: UpdatePasswordForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UpdatePasswordForm>;
+
+export const UpdatePassword: Story = {
+  args: {
+    initialValues: {
+      email: '',
+    },
+  },
+  render: () => <UpdatePasswordForm />,
+};
