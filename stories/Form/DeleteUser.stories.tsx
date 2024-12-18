1ff6e41be90c28691f6e2d99d7378710619ecbde
ciscutiez <francischarlesi.gonzales@gmail.com>
2024-12-18 18:17:59 +0800

    feat: delete user storybook

---
+++ stories/Form/DeleteUser.stories.tsx
@@ -0,0 +1,30 @@
+import DeleteUserForm from '@/components/Form/DeleteUser';
+import { Card } from '@/components/ui/card';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof DeleteUserForm> = {
+  title: 'Forms/Delete User Form',
+  component: DeleteUserForm,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DeleteUserForm>;
+
+export const DeleteUser: Story = {
+  // args: {
+  //   initialValues: {
+  //     email: '',
+  //   },
+  // },
+  render: () => (
+    <Card className=''>
+      <DeleteUserForm />
+    </Card>
+  ),
+};
